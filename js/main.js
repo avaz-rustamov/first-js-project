ff1fd5f5a583d8f27a5481fb05463da8d71a59ad
avaz-dev <avaz.dev@icloud.com>
2021-09-08 11:26:58 +0500

    Add Survey

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-var namee = ("Avaz Frontend Developer")
-console.log("Avaz Rustamov")
+var namee = "AvazFrontend Developer"
+var age = null
+var interest = null
+var info =null
+
+alert("Najot Ta'limga Xush Kelibsiz...")
+var namee = prompt("Ismingizni kiriting...", "Aziz")
+var age = prompt("Yoshingiz kiriting...", "18")
+var interest = prompt("Qaysi yo'nalishga qiziqmoqdasiz... (,Frontend,Backend,Grafik Dizayn)", "SMM")
+var info = prompt("Biz haqimizda qayerdan eshitdingiz", "Telegram")
+console.log("Ism: ")
 console.log(namee)
-console.log(namee)
-console.log(namee)
-console.log(namee)
-console.log(namee)
-console.log(namee)
+console.log("Yosh: ")
+console.log(age)
+console.log("Qiziqish: ")
+console.log(interest)
+console.log("Biz Haqimizda: ")
+console.log(info)
